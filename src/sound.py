950858939a2a4452817a7de62e30bee4372fa9e9
adamiaonr <adamiaonr@gmail.com>
2022-06-12 17:51:31 +0100

    added src/ directory and sound + xeno-canto utility functions

---
+++ src/sound.py
@@ -0,0 +1,40 @@
+import librosa as lr
+import soundfile as sf
+import noisereduce as nr
+
+from pathlib import Path
+
+def _get_file_path(input_file_path:Path, suffix:str, output_dir:Path = None, format = 'WAV') -> Path:
+    # file name format : add '{suffix}' to the 'name' of input_filepath
+    file_name = f"{input_file_path.stem}_{suffix}.{format}".lower()
+    # if no output dir is specified, save file in same directory
+    if output_dir:
+      output_file_path = output_dir / file_name
+    else:
+      output_file_path = input_file_path.with_stem(file_name)
+
+    return output_file_path
+
+def split_into_non_silent(input_file_path:Path, output_dir:Path = None, 
+  threshold:list = [.5, 1.], format = 'WAV', force = False):
+  # load audio file and split into non-silent intervals
+  waveform, sample_rate = lr.load(path = input_file_path)
+  intervals = lr.effects.split(y = waveform)
+  # save intervals w/ duration within threshold[] seconds
+  for i, intr in enumerate(intervals):
+    duration = lr.get_duration(y = waveform[intr[0]:intr[1]], sr = sample_rate)
+    if duration < threshold[0] or duration > threshold[1]:
+      continue
+    output_file_path = _get_file_path(input_file_path, f'{i}', output_dir, format)
+    if (not output_file_path.is_file()) or force:
+      sf.write(output_file_path, waveform[intr[0]:intr[1]], sample_rate, format = format)
+
+def reduce_background_noise(input_file_path:Path, output_dir:Path = None, format = 'WAV', force = False):
+  # output file name format : add '_noisered' suffix to input_filename
+  output_file_path = _get_file_path(input_file_path, 'noisered', output_dir, format)
+  # skip noise removal if file already exists or if force = False
+  if (not output_file_path.is_file()) or force:
+    waveform, sample_rate = lr.load(input_file_path)
+    # apply non-stationary noise reduction to audio file (https://pypi.org/project/noisereduce/)
+    waveform_reduced = nr.reduce_noise(y = waveform, sr = sample_rate)
+    sf.write(output_file_path, waveform_reduced, sample_rate, format = format)
---
+++ src/xenocanto.py
@@ -0,0 +1,67 @@
+import time
+import wget
+import typing
+import requests
+
+from pathlib import Path
+from urllib.parse import quote
+
+XENO_CANTO_NAME = "xeno-canto"
+XENO_CANTO_API_ENDPOINT = "https://www.xeno-canto.org/api/2"
+
+def get_recordings_list(genus:str, subspecies:str) -> dict:
+  # sanitize query (contains spaces)
+  query = quote(f'query=ssp:"{subspecies}" gen:"{genus}"', safe = '=:')
+  url = f"{XENO_CANTO_API_ENDPOINT}/recordings?{query}"
+  
+  response = requests.get(url, verify = True)
+  
+  try:
+    records_list = response.json()
+  except requests.exceptions.JSONDecodeError:
+    records_list = {}
+
+  return records_list
+
+def get_recording_filename(recording:dict) -> str:
+    try:
+      # audio files saved w/ format {genus}_{species}_{subspecies}_{id}.{ext}
+      # in this case, {ext} shall be an audio format (e.g., 'mp3', 'wav')
+      file_name = "{genus}_{species}_{subspecies}_{id}{ext}".format(
+        genus       = recording['gen'],
+        species     = recording['sp'], 
+        subspecies  = recording['ssp'],
+        id          = recording['id'],
+        ext         = Path(recording['file-name']).suffix
+      )
+    except KeyError:
+      raise KeyError("error while generating filename for recording")
+    
+    return file_name.lower()
+
+def download_recordings(recordings_list:dict, output_dir:typing.Union[str, Path], 
+  max_records:int = 0, wait:int = 1):
+
+  if 'recordings' not in recordings_list:
+    raise KeyError("no audio recordings on records list ('recordings' key is missing)")
+
+  if wait < 1:
+    raise ValueError(f"wait < 1 seconds not allowed : would overload {XENO_CANTO_NAME} API")
+
+  max_records = max_records if max_records > 0 else len(recordings_list['recordings'])
+  for i, rec in enumerate(recordings_list['recordings'][:max_records]):
+    try:
+      # url to download the audio file
+      url = rec['file']
+      # audio should be saved with format {genus}_{species}_{subspecies}_{id}.{ext}
+      file_name = get_recording_filename(rec)
+    except KeyError:
+      pass
+    else:
+      # do not download if file already exists
+      file_path = Path(output_dir) / file_name
+      if not file_path.is_file():
+        wget.download(url, str(file_path))
+      
+      # sleep to avoid xeno-canto API overload
+      time.sleep(wait)
